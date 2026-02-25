eed6acb12c8af46c90006a13bbf45c65c53754fd
CODED BLACK <codedb693@gmail.com>
2026-02-25 20:18:39 +0300

    Update asgi.py

--- myproject/asgi.py
+++ myproject/asgi.py
@@ -3,7 +3,6 @@ ASGI config for myproject project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
-
 For more information on this file, see
 https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 """
